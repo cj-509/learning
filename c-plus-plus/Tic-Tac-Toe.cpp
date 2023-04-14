72c5cf66e05f9e64875f8209749f2512cfb1686f
cj <jb.carl@outlook.com>
2023-04-13 21:44:42 -0600

    .

---
+++ c-plus-plus/Tic-Tac-Toe.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+char position[3][3] = {
+	{'1', '2', '3'},
+	{'4', '5', '6'},
+	{'7', '8', '9'}
+};
+
+int main(){
+	cout<<position[2][0];
+	return 0;
+
+}
+
