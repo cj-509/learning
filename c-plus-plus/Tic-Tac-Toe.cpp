3bf36fe9b9a671c6cf41cdf1c3f3e4ffd0b445fb
cj <jb.carl@outlook.com>
2023-04-13 23:57:58 -0600

    .

--- c-plus-plus/Tic-Tac-Toe.cpp
+++ c-plus-plus/Tic-Tac-Toe.cpp
@@ -7,10 +7,27 @@ char position[3][3] = {
 	{'4', '5', '6'},
 	{'7', '8', '9'}
 };
+void board();
 
 int main(){
-	cout<<position[2][0];
+	cout <<postion[2][0]<<endl;
+	
+	board();
 	return 0;
 
+}
+
+
+
+void board(){
+	cout <<"     "<<"   |   "<<"  |  "<<endl;
+	cout <<"     "<<position[0][0] << "  |  "<<position[0][1] << "  |  " <<position[0][2]<<endl;
+	cout <<"   __"<<"___|___"<<"__|_____"<<endl;
+	cout <<"     "<<"   |   "<<"  |  "<<endl;
+	cout <<"     "<<position[1][0] << "  |  "<<position[1][1] << "  |  " <<position[1][2]<<endl;
+	cout <<"   __"<<"___|___"<<"__|_____"<<endl;
+	cout <<"     "<<"   |   "<<"  |  "<<endl;
+	cout <<"     "<<position[2][0] << "  |  "<<position[2][1] << "  |  " <<position[2][2]<<endl;
+	cout <<"     "<<"   |   "<<"  |  "<<endl;
 }
 
